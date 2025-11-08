a8055d21738b6d38b9a45010e8c8e7a5eb880d2d
Sebastian Luna <eliaslunaslo@gmail.com>
2025-11-08 19:57:59 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿//# Adding a new task
+//task - cli add "Buy groceries"
+//# Output: Task added successfully (ID: 1)
+
+//# Updating and deleting tasks
+//task-cli update 1 "Buy groceries and cook dinner"
+//task-cli delete 1
+
+//# Marking a task as in progress or done
+//task-cli mark-in-progress 1
+//task-cli mark-done 1
+
+//# Listing all tasks
+//task-cli list
+
+//# Listing tasks by status
+//task-cli list done
+//task-cli list todo
+//task-cli list in-progress
+
+bool activeApp = true;
+
+string[] states = new string[] {
+    "todo", "in-progress", "done" };
+
+string[] commands = new string[] { 
+    "add", "update", "delete", "list",
+    "mark-in-progress", "mark-done" };
+
+/* Main application loop */
+
+while (activeApp) {
+    ReadLineInput();
+
+    activeApp = false;
+}
+
+string ReadLineInput() { 
+    string input = Console.ReadLine();
+
+    input = input.Trim();
+    string command = input.Split(' ')[0].ToLower();
+
+    if (commands.Contains(command))
+    {
+        if (command == commands[0])
+        {
+            string title = input.Substring(command.Length).Trim();
+            string msg = "";
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(title))
+                {
+                    throw new ArgumentNullException(nameof(title), "Task title cannot be empty.");
+                } 
+                else if (!title.StartsWith('"') && !title.EndsWith('"'))
+                {
+                    throw new ArgumentNullException(nameof(title), "Task title cannot start or ends without quotation marks");
+                }
+                else
+                {
+                    msg = AddTask(title);
+                }
+            }
+            catch (ArgumentNullException argEx)
+            {
+                msg = argEx.Message;
+            }
+
+            Console.WriteLine(msg);
+        }
+    }
+
+
+    return input;
+
+}
+
+string ReturnList() { 
+ return "";
+}
+string AddTask(string title) {
+    if (true)
+    {
+        
+    }
+    return title;
+}
+
+string UpdateTask(int id, string newTitle) {
+    return "";
+}
+string SetState() {
+    return "";
+}
---
+++ Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TaskTracker
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; } // e.g., "todo", "in-progress", "done"
+
+        
+    }
+}
